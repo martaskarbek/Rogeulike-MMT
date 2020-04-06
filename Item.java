18a4c101bfec53e4d2c63aa7b5487376df416a1d
Marta Skarbek <marta@skarbek.name>
2020-04-06 15:59:22 +0200

    added items constructor

--- Item.java
+++ Item.java
@@ -1,4 +1,11 @@
-
 public abstract class Item {
 
+    private String itemLook;
+
+    public Item(String itemLook) {
+        this.itemLook = itemLook;
+    }
+
+
+    public abstract void changePoints(Player player);
 }
