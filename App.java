07c91a1866cbadd30a6294d29ff2ef6f9f31a3b5
Tomek Krogulec <tomek.krogulec@o2.pl>
2020-04-07 12:31:13 +0200

    added board

--- App.java
+++ App.java
@@ -3,14 +3,14 @@ import javax.swing.JTextField;
 
 public class App {
     public static void main(String[] args) {
-        // JTextField textField = new JTextField();
+        JTextField textField = new JTextField();
 
-        // textField.addKeyListener(new Game());
-        // JFrame jframe = new JFrame();
+        textField.addKeyListener(new Game());
+        JFrame jframe = new JFrame();
 
-        // jframe.add(textField);
-        // jframe.setSize(100, 100);
-        // jframe.setVisible(true);
+        jframe.add(textField);
+        jframe.setSize(100, 100);
+        jframe.setVisible(true);
 
         Board board = new Board();
         board.printBoard();
