f5bef9b52fd41ca09176c593f2212ba473874f8d
Marta Skarbek <marta@skarbek.name>
2020-04-08 07:13:50 +0200

    added class for create board

---
+++ Board.java
@@ -0,0 +1,27 @@
+public class Board {
+    private Square[][] gameBoard;
+
+    public Board(String[][] lalala) {
+        int rows = 30, columns = 70;
+        gameBoard = new Square[rows][columns];
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                gameBoard[row][column] = new Square(lalala[row][column]);
+            }
+        }
+    }
+
+
+    public void printBoard() {
+        for (Square[] row : gameBoard) {
+            for (Square square : row) {
+                System.out.print(square);
+            }
+            System.out.println();
+        }
+    }
+
+    public Square[][] getGameBoard() {
+        return gameBoard;
+    }
+}
