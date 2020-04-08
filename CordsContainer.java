73599cc97ac76554cfdbda9adbf4be83f0767c35
Marta Skarbek <marta@skarbek.name>
2020-04-08 07:13:29 +0200

    created constructor

--- CordsContainer.java
+++ CordsContainer.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class CordsContainer {
+    private List<Coordinates> coordinates = new ArrayList<>();
 
+    //public CordsContainer() {
+
+    //}
+
+    public void addCoordinates(Coordinates coord, List<Coordinates> coordinates) {
+        coordinates.add(coord);
+    }
 }
