7cc1e72a2c37342056751cdbe672d0bd91f99afb
Marta Skarbek <marta@skarbek.name>
2020-04-08 07:16:23 +0200

    added class for create map squares objects

---
+++ Square.java
@@ -0,0 +1,19 @@
+class Square {
+    private String look;
+
+    public Square(String look) {
+        this.look = look;
+    }
+
+    public String toString() {
+        return look;
+    }
+
+    public String getLook() {
+        return look;
+    }
+
+    public String setLook(String look) {
+        return this.look = look;
+    }
+}
