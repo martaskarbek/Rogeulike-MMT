f613ecbb77aeeddd29050938e58bc8fd96be7856
Marta Skarbek <marta@skarbek.name>
2020-04-09 12:56:42 +0200

    added abstract class to create enemies

--- Enemy.java
+++ Enemy.java
@@ -1,4 +1,18 @@
 
-public class Enemy {
+public abstract class Enemy {
+    Coordinates pivot;
+    String sign;
 
+    public Enemy(Coordinates pivot, String sign) {
+        this.pivot = pivot;
+        this.sign = sign;
+    }
+
+    public Coordinates getPivot(){
+        return this.pivot;
+    }
+
+    public String getSymbol() {
+        return this.sign;
+    }
 }
